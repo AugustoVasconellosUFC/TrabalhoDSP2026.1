08cf2ef03c566428036d51dee64fc0657bf58947
Augusto Vasconcelos <augustovnd@alu.ufc.br>
2026-03-31 15:32:20 -0300

    atualizando main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    nome: str
+    preco: float
+    vendedor: str   # Em futuras implementações pode ser mudado para int (id do vendedor)
+    estoque: int
+
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return{"msg":"Olá, mundo!"}
